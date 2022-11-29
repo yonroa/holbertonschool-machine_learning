1cd0137e48730f697e46888129a3589a2c6a5abd
yonroa <yonanderson11@gmail.com>
2022-11-29 12:42:06 -0500

    Add sum arrays function

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Contains the 'add_arrays' function"""
+
+
+def add_arrays(arr1, arr2):
+    """adds two arrays element-wise"""
+    if len(arr1) != len(arr2):
+        return None
+    sum_array = []
+    for i in range(len(arr1)):
+        sum_array.append(arr1[i] + arr2[i])
+    return sum_array
