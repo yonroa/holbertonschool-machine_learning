e06e4d1f39758fc3eeeb2a67cc830a6329553a17
yonroa <yonanderson11@gmail.com>
2022-12-01 15:54:52 -0500

    Add ops numpy

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Contains the 'np_elementwise' function"""
+
+
+def np_elementwise(mat1, mat2):
+    """Performs element-wise addition, subtraction,
+    multiplication, and division
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
