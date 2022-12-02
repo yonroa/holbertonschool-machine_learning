303baad06d7a04f2703c66a2ef29bca1b69a66e5
yonroa <yonanderson11@gmail.com>
2022-12-02 11:12:54 -0500

    Fix pycodestyle

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -17,20 +17,3 @@ def cat_arrays(arr1, arr2):
     new_array = arr1.copy()
     new_array.extend(arr2)
     return new_array
-
-
-if __name__ == "__main__":
-    m1 = [[51, 24, 73], [93, 45, 77]]
-    m2 = [[], [], []]
-    m = cat_matrices2D(m1, m2)
-    print(m)
-    m2 = [[51, 24, 73], [93, 45, 77]]
-    m1 = [[], [], []]
-    m = cat_matrices2D(m1, m2, axis=0)
-    print(m)
-    m1 = [[75, 23, 58], [32, 5, 67], [34, 65, 22]]
-    m2 = [[], [], []]
-    m = cat_matrices2D(m1, m2, axis=1)
-    if type(m) is not list or m is m1 or m is m2 or not len(m) or type(m[0]) is not list:
-        print("Not a new matrix")
-    print(m)
