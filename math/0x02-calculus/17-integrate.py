2dfbbd36c06ade252cd987bde66a0c881fd8ad83
yonroa <yonanderson11@gmail.com>
2022-12-08 18:01:21 -0500

    C is integer

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,7 +6,7 @@ def poly_integral(poly, C=0):
     """Calculates the integral of a polynomial"""
     if isinstance(poly, list) and all(
             isinstance(x, int) for x in poly) and len(
-                poly) > 0 and isinstance(C, (int, float)):
+                poly) > 0 and isinstance(C, int):
         integrate = []
         integrate.append(C)
         for i in range(len(poly)):
