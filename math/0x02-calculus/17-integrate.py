80c20e477448e45946f5bac1eac213250dbd8fe3
yonroa <yonanderson11@gmail.com>
2022-12-08 18:24:39 -0500

    Change function

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,14 +4,15 @@
 
 def poly_integral(poly, C=0):
     """Calculates the integral of a polynomial"""
-    if isinstance(poly, list) and all(
-            isinstance(x, int) for x in poly) and len(
-                poly) > 0 and isinstance(C, (int, float)):
+    if poly and isinstance(poly, list) and (
+            isinstance(C, int) or isinstance(C, float)) and all(
+            isinstance(x, (int, float)) for x in poly):
+        if poly == [0]:
+            return[C]
         integrate = []
         integrate.append(C)
         for i in range(len(poly)):
-            exp = i + 1
-            coef = poly[i] / exp
+            coef = poly[i] / (i + 1)
             if coef.is_integer():
                 integrate.append(int(coef))
             else:
