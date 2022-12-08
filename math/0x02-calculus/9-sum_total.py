761d75d4b5b3d1f4ea846d971cacc61073f8d424
yonroa <yonanderson11@gmail.com>
2022-12-07 19:14:14 -0500

    Change condition

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,9 +4,9 @@
 
 def summation_i_squared(n):
     """Calculates the summatory of i^2"""
-    if type(n) != int or n < 0:
-        return None
-    return int((n / 6) * (n + 1) * (2 * n + 1))
+    if isinstance(n, int) and n > 0:
+        return int((n / 6) * (n + 1) * (2 * n + 1))
+    return None
 
 
 if __name__ == "__main__":
