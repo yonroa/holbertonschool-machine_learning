4607545e4c4f41981b46ab50f3c7912be60b8003
yonroa <yonanderson11@gmail.com>
2022-12-08 18:17:15 -0500

    Add len poly

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -5,7 +5,8 @@
 def poly_integral(poly, C=0):
     """Calculates the integral of a polynomial"""
     if isinstance(poly, list) and all(
-            isinstance(x, int) for x in poly) and isinstance(C, (int, float)):
+            isinstance(x, int) for x in poly) and len(
+                poly) > 0 and isinstance(C, (int, float)):
         integrate = []
         integrate.append(C)
         for i in range(len(poly)):
