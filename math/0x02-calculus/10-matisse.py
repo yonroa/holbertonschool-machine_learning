d78e2c094e8df9a962aa71a2ce39d5b6d1d6c68f
yonroa <yonanderson11@gmail.com>
2022-12-07 19:22:18 -0500

    Add derivates

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Contains the 'poly_derivative' function"""
+
+
+def poly_derivative(poly):
+    """Calculates the derivative of a polynomial"""
+    derivate = []
+    for i in range(len(poly)):
+        exp = i - 1
+        coef = poly[i] * i
+        if exp >= 0:
+            derivate.append(coef)
+    return derivate
+
+
+if __name__ == "__main__":
+    poly = [1, -5, 0, 3, 1]
+    print(poly_derivative(poly))
