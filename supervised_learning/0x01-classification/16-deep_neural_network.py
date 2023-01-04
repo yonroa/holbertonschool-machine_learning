f94b3daf722bc892c0e71bddb5ee187d7f7d4bcd
yonroa <yonanderson11@gmail.com>
2023-01-03 20:30:24 -0500

    Update checks

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -19,7 +19,7 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) != list or len(list) == []:
+        if type(layers) != list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
         negative = list(filter(lambda x: x <= 0, layers))
         if len(negative) > 0:
