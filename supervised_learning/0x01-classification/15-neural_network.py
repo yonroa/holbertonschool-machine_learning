b247c3b6943652437f2423dce88bccc256870bfe
yonroa <yonanderson11@gmail.com>
2023-01-04 22:06:43 -0500

    Change in sums

--- supervised_learning/0x01-classification/15-neural_network.py
+++ supervised_learning/0x01-classification/15-neural_network.py
@@ -114,10 +114,10 @@ class NeuralNetwork:
         m = len(Y[0])
         Dz2 = A2 - Y
         Dw2 = (1 / m) * np.matmul(A1, Dz2.T)
-        Db2 = (1 / m) * np.sum(Dz2)
+        Db2 = (1 / m) * np.sum(Dz2, axis=1, keepdims=True)
         Dz1 = np.matmul(self.__W2.T, Dz2) * (A1 * (1 - A1))
         Dw1 = (1 / m) * np.matmul(X, Dz1.T)
-        Db1 = (1 / m) * np.sum(Dz1)
+        Db1 = (1 / m) * np.sum(Dz1, axis=1, keepdims=True)
 
         self.__W1 = self.__W1 - (alpha * Dw1.T)
         self.__b1 = self.__b1 - (alpha * Db1)
