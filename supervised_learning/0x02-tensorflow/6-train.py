aee227ba82c40d2648b675f08d3d825944ce5375
yonroa <yonanderson11@gmail.com>
2023-01-06 14:37:59 -0500

    Update imports

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -2,11 +2,11 @@
 """Contains the function 'train'"""
 
 import tensorflow.compat.v1 as tf
-create_placeholders = __import__('0-create_placeholders').create_placeholders
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-forward_prop = __import__('2-forward_prop').forward_prop
 calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
