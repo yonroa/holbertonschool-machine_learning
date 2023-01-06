ed05595e55a1addb0a51a7bf0e3c85d79596b67c
yonroa <yonanderson11@gmail.com>
2023-01-06 14:47:56 -0500

    Update task

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -13,16 +13,16 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
           activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """Builds, trains, and saves a neural network classifier"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_pred = forward_prop(x, layer_sizes, activations)
-    loss = calculate_loss(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-    train_op = create_train_op(loss, alpha)
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
+    y_pred = forward_prop(x, layer_sizes, activations)
     tf.add_to_collection('y_pred', y_pred)
+    loss = calculate_loss(y, y_pred)
     tf.add_to_collection('loss', loss)
+    accuracy = calculate_accuracy(y, y_pred)
     tf.add_to_collection('accuracy', accuracy)
-    tf.add_to_collection('train_op', train_op)
+    train_op = create_train_op(loss, alpha)
+    tf.add_to_collection("train_op", train_op)
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
