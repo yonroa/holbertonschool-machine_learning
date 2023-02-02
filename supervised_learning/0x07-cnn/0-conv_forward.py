da489969b4444a33efd3a6227c25def8973360e3
yonroa <yonanderson11@gmail.com>
2023-02-02 17:31:02 -0500

    Update task

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -30,7 +30,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         p_h, p_w = padding
 
     images = np.pad(A_prev, ((0, 0), (p_h, p_h),
-                    (p_w, p_w), (0, 0)), 'constant')
+                    (p_w, p_w), (0, 0)), mode='constant', constant_values=0)
     h_out = ((h_prev - kh + 2 * p_h) // sh) + 1
     w_out = ((w_prev - kw + 2 * p_w) // sw) + 1
     conv = np.zeros((m, h_out, w_out, c_new))
@@ -38,8 +38,9 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     for i in range(c_new):
         for j in range(h_out):
             for k in range(w_out):
-                p_images = images[:, sh * j: sh * j + kh, sw * k: sw * j + kw]
-                conv[:, j, k, i] = np.tensordot(
-                    p_images, W[:, :, :, i], axes=3) + b[:, :, :, i]
+                p_images = np.sum(np.multiply(
+                    images[:, sh * j: sh * j + kh, sw * k: sw * k + kw],
+                    W[:, :, :, i]), axis=(1, 2, 3))
+                conv[:, j, k, i] = activation((p_images + b[:, :, :, i]))
 
-    return activation(conv)
+    return conv
