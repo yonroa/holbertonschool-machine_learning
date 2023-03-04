7334ad662cc747cad8e6101874199aecd9f7da91
yonroa <yonanderson11@gmail.com>
2023-03-04 14:45:30 -0500

    Reverse task

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-"""
-YOLO(You Only Look Once !!!)
-"""
+"""Contain the class 'Yolo'"""
+
 import numpy as np
 import tensorflow.keras as K
 
 
+def sigmoid(number):
+    """Sigmoid activation function"""
+    return 1 / (1 + np.exp(-number))
+
+
 class Yolo:
-    """
-    YOLO V3
-    """
+    """Uses the Yolo v3 algorithm to perform object detection"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """
+        """Initialize the class Yolo
+
         Args:
             model_path: is the path to where a Darknet Keras model is stored
             classes_path: is the path to where the list of class names used for
@@ -21,88 +24,50 @@ class Yolo:
                      filtering step
             nms_t: representing the IOU threshold for non-max suppression
             anchors:(outputs,anchor_boxes,2)containing all of the anchor boxs
-                outputs: is the number of outputs (predictions)
-                         made by the Darknet model
-                anchor_boxes: number of anchor boxes used for each predic
-                2 => [anchor_box_width, anchor_box_height]
         """
-        # Loading model with Keras
         self.model = K.models.load_model(model_path)
-        # Character   Meaning
-        # 'r'   open for reading (default)
-        # 'w'   open for writing, truncating the file first
-        # 'x'   open for exclusive creation, failing if the file already exists
-        # 'a'   open for writing, appending to the end of the file if it exists
-        # 'b'   binary mode
-        # 't'   text mode (default)
-        # '+'   open a disk file for updating (reading and writing)
-        # '
-        # U'   universal newlines mode (deprecated)
-        with open(classes_path, 'rt') as fd:
-            self.class_names = fd.read().rstrip('\n').split('\n')
+        with open(classes_path, 'rt') as file:
+            self.class_names = file.read().rstrip('\n').split('\n')
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
 
-    def sigmoid(self, number):
-        """
-        Sigmoid activation function
-        """
-        return 1 / (1 + np.exp(-number))
-
     def process_outputs(self, outputs, image_size):
         """
-           Args:
-             outputs: numpy.ndarray - contains predictions from model
-               for single image.
-             image_size: numpy.ndarray - images original
-               size (image_height, image_width)
-           Return:
-              tuple - (boxes, box_confidence, box_class_probs)
-              boxes: numpy.ndarray - (grid_height, grid_width, anchorboxes, 4)
-                 4 => (x1, y1, x2, y2)
-              box_confidence: numpy.ndarray - shape
-                (grid_height, grid_width, anchor_boxes, 1)
-              box_class_probs: numpy.ndarray - shape
-                (grid_height, grid_width, anchor_boxes, classes)
-                contains class probabilities for each output
+        Args:
+            outputs: contains the predictions from the Darknet
+                model for a single image
+            image_size: contains the image's original size
         """
-        IH, IW = image_size[0], image_size[1]
+        h, w = image_size[0], image_size[1]
         boxes = [output[..., :4] for output in outputs]
-        box_confidence, class_probs = [], []
-        cornersX, cornersY = [], []
+        confidence, probs = [], []
+        cornerX, cornerY = [], []
 
         for output in outputs:
-            # Organize grid cells
             gridH, gridW, anchors = output.shape[:3]
             cx = np.arange(gridW).reshape(1, gridW)
             cx = np.repeat(cx, gridH, axis=0)
             cy = np.arange(gridW).reshape(1, gridW)
             cy = np.repeat(cy, gridH, axis=0).T
 
-            cornersX.append(np.repeat(cx[..., np.newaxis], anchors, axis=2))
-            cornersY.append(np.repeat(cy[..., np.newaxis], anchors, axis=2))
-            # box confidence and class probability activations
-            box_confidence.append(self.sigmoid(output[..., 4:5]))
-            class_probs.append(self.sigmoid(output[..., 5:]))
+            cornerX.append(np.repeat(cx[..., np.newaxis], anchors, axis=2))
+            cornerY.append(np.repeat(cy[..., np.newaxis], anchors, axis=2))
+            confidence.append(sigmoid(output[..., 4:5]))
+            probs.append(sigmoid(output[..., 5:]))
 
         inputW = self.model.input.shape[1]
         inputH = self.model.input.shape[2]
 
-        # Predicted boundary box
         for x, box in enumerate(boxes):
-            bx = ((self.sigmoid(box[..., 0])+cornersX[x])/outputs[x].shape[1])
-            by = ((self.sigmoid(box[..., 1])+cornersY[x])/outputs[x].shape[0])
+            bx = ((sigmoid(box[..., 0])+cornerX[x])/outputs[x].shape[1])
+            by = ((sigmoid(box[..., 1])+cornerY[x])/outputs[x].shape[0])
             bw = ((np.exp(box[..., 2])*self.anchors[x, :, 0])/inputW)
             bh = ((np.exp(box[..., 3])*self.anchors[x, :, 1])/inputH)
 
-            # x1
-            box[..., 0] = (bx - (bw * 0.5))*IW
-            # y1
-            box[..., 1] = (by - (bh * 0.5))*IH
-            # x2
-            box[..., 2] = (bx + (bw * 0.5))*IW
-            # y2
-            box[..., 3] = (by + (bh * 0.5))*IH
+            box[..., 0] = (bx - (bw * 0.5))*w
+            box[..., 1] = (by - (bh * 0.5))*h
+            box[..., 2] = (bx + (bw * 0.5))*w
+            box[..., 3] = (by + (bh * 0.5))*h
 
-        return (boxes, box_confidence, class_probs)
+        return (boxes, confidence, probs)
