319814a62bec0892321ac85a0ab1f261ea7be049
yonroa <yonanderson11@gmail.com>
2023-03-13 18:21:50 -0500

    Fix error check

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -11,7 +11,7 @@ class MultiNormal:
         """Initialize the class MultiNormal"""
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        if data.shape[0] < 2:
+        if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
         d, n = data.shape
         self.mean = np.mean(data, axis=1, keepdims=True).reshape(d, 1)
