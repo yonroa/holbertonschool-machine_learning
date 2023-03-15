7016c082cac150b68a6a9fe7a2e4b89d6f60413c
yonroa <yonanderson11@gmail.com>
2023-03-15 14:23:59 -0500

    Fix task

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -6,13 +6,8 @@ import numpy as np
 
 def pca(X, ndim):
     """performs PCA on a dataset"""
-    mean = np.mean(X, axis=0)
-    centered_data = X - mean
-    covariance_matrix = np.cov(centered_data.T)
-    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[sorted_indices]
-    eigenvectors = eigenvectors[:, sorted_indices]
-    transformation_matrix = eigenvectors[:, :ndim]
-    transformed_data = np.dot(centered_data, transformation_matrix)
-    return transformed_data
+    x_mean = X - np.mean(X, axis=0)
+    u, s, vh = np.linalg.svd(x_mean)
+    W = vh.T[:, :ndim]
+    T = np.matmul(x_mean, W)
+    return T
